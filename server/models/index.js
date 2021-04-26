f4cabaac297b6bb08e42a9c2b51b8399545617cd
Abhijeet Padwal <38448205+padwalab@users.noreply.github.com>
2021-04-25 23:14:26 -0700

    connecitno pooloign

--- server/models/index.js
+++ server/models/index.js
@@ -10,6 +10,10 @@ class Database {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: true,
+        poolSize: 10, // Maintain up to 10 socket connections
+        serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+        family: 4,
       })
       .then(() => {
         console.log("Database connection successful");
